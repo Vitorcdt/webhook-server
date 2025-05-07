b017def9e4524f6869a04530c3f9ae2cb2df6f64
Vitorcdt <vitorcartaodetodos@gmail.com>
2025-05-07 18:53:33 +0000

    make v5

--- index.ts
+++ index.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 
 const supabase = createClient(
   process.env.SUPABASE_URL!,
-  process.env.SUPABASE_KEY!
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
 );
 
 app.get('/webhook', (req: Request, res: Response) => {
